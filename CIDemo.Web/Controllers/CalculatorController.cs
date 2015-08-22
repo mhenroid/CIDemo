f0c898e1e3a7fdbdfd74edd3345836bb6a6348ae
Matthew Henroid <matthewhenroid@gmail.com>
2015-08-22 16:29:51 -0500

    Adding initial calculator page to the web project.

---
+++ CIDemo.Web/Controllers/CalculatorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CIDemo.Web.Models;
+
+namespace CIDemo.Web.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var viewModel = new CalculatorViewModel();
+            viewModel.N = 0;
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Index(CalculatorViewModel viewModel)
+        {
+            viewModel.Result = "Test 123";
+            return View(viewModel);
+        }
+    }
+}
---
+++ CIDemo.Web/Models/CalculatorViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CIDemo.Web.Models
+{
+    public class CalculatorViewModel
+    {
+        public int N { get; set; }
+        public string Result { get; set; }
+    }
+}
